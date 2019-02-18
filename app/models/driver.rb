7f8b527a521f098d12535f6ec30e126c986cde27
bburnett86 <breton.burnett@gmail.com>
2019-02-18 15:53:53 -0600

    Use include instead of join for queries

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,6 +3,6 @@ class Driver < ApplicationRecord
 
   def routes_per_day(delivery_date_string)
     d_date = DateTime.strptime(delivery_date_string, "%m/%d/%Y")
-    Driver.joins(:routes).where(routes: {expected_delivery_date: d_date}).where(id: self.id).count
+    Driver.includes(:routes).where(routes: {expected_delivery_date: d_date}).where(id: self.id).count
   end
 end
